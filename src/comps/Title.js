98b48377fa811beb4cd57f3c51bb412f32535e2f
dimasrifalta <dimasrifalta@gmail.com>
2021-01-03 22:37:12 +0700

    Ganti style layout

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,7 +4,7 @@ const Title = () => {
   return (
     <div className='title'>
       <h1>MyMoments</h1>
-      <h2>Your Moment</h2>
+      <h2>Your Moments</h2>
       <p>MyMoments Adalah media untuk menampilkan foto di hidup saya.</p>
     </div>
   );
